3472bae0a974e89212726dd819ed9b03de6f9879
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-03-14 08:36:04 +0100

    Simple edit

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var http = require('http');
-const fs = require('fs');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-fs.readFile('index.html', function(err, data) {
-    res.write(data);
-    res.end();
-});
-
-//res.end();
-}).listen(8080);
+console.log('Hello, World!');
