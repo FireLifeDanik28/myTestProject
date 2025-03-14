3d8d9a551371d8a9df3009d60cb775ce45645be8
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-03-14 09:41:58 +0100

    xd

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log('Zadanie: ', req.method, req.url);
+    next();
+});
+
 app.get('/', (req, res) => {
+    console.log('Zadanie GET dla strony glownej' , req.query);
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    console.log('Zadanie POST dla strony glownej' , req);
     res.send('Hello World!');
 });
 
