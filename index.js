f28d21b2a8bf39b9724ff8e39321fa3734392880
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-03-28 09:36:37 +0100

    nie działa :(

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
+const express = require('express');
 const pg = require('pg');
-
-// Create a new client instance
+const app = express();
 const client = new pg.Client({
-  host: '192.168.0.207',
-  port: 5432,
-  user: 'user14',
-  password: '7SAZONQZ',
-  database: 'user14_db'
+    host: '192.168.0.207',
+    port: 5432,
+    user: 'user14',
+    password: '7SAZONQZ',
+    database: 'user14_db'
+})
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+//app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-
-const getFromDb = async () => {
-    
-    const result = await client.query('SELECT * FROM grades');
+app.get('/grades/:student_id/', async (req, res) => {
+    const studentId = req.params.student_id;
+    console.log("Student ID:", studentId);
+    const query = 'SELECT * FROM grades WHERE student_id = ' + studentId;
+    const result = await client.query(query);
+    console.log("Query Result:");
     console.table(result.rows);
-}
-
-const insertToDb = async (student_id, course_id, grade) => {
-    await client.connect();
-    student_id = prompt("Podaj student id: ");
-    course_id = prompt("Podaj id przedmiotu: ");
-    grade = prompt("Podaj ocene: ");
-    const result = await client.query("INSERT INTO grades (" + student_id + ", " + course_id + ", " + grade + ") VALUES(1, 1, 5);")
-}
-client.connect().then(async () => {
-    await insertToDb();
-    await getFromDb();
-}).then
-await client.end();
+    await client.end();
+    res.end();
+});
+app.post('/grades/:student_id/', async (req, res) => {
+    const studentId = req.params.student_id;
+    console.log("Student ID:", studentId);
+    console.log("Request Body:", req.body);
+    res.end();
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}); 
